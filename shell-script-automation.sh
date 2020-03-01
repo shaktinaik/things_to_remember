f0eba94a7fb534e20750d5d3fc23f9253b8d170e
Shakti Naik <34809458+shaktinaik@users.noreply.github.com>
2020-03-01 16:08:40 +0530

    Update shell-script-automation.sh

--- shell-script-automation.sh
+++ shell-script-automation.sh
@@ -5,3 +5,12 @@ curl -k "sftp://${SFTP_USER_ID}@${SFTP_HOSTNAME}/${SFTP_FILE_PATH}/${FILE_NAME}"
 
 #Password less login is not enabled
 
+
+#Password less login to any server from loacl machine
+#In the terminal of local machine type the below command 
+
+ssh-keygen -t rsa
+ssh <user_id>@<machine_id> mkdir -p .ssh
+cat .ssh/id_rsa.pub | ssh <user_id>@<machine_id> 'cat >> .ssh/authorized_keys'
+ssh <user_id>@<machine_id> "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
+
